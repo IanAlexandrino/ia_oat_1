581ccf1a783243029739f7bf2c8bba84ebf0fdfc
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-23 13:17:41 -0300

    Consertando método insere_dados criando método conta_repetição e finalizando método main

--- problema_2/app.py
+++ problema_2/app.py
@@ -1,17 +1,28 @@
 import random
 
 def insere_dados(lista):
-    if not lista:
-        return 0
     while len(lista) < 100:
-        lista.append(random.randint(1,100))
+        lista.append(random.randint(1, 50))
+
+def conta_repeticao(lista, num):
+    numero_contagem = 0
+
+    for numero in lista:
+        if numero == num:
+            numero_contagem += 1
+
+    return numero_contagem
 
 def main():
     lista_dados = []
 
     insere_dados(lista_dados)
 
-    
+    print(lista_dados)
+
+    numero_usuario = int(input("Insira um número até 50 para procurar na lista de dados: "))
+
+    print(f"O número {numero_usuario} aparece {conta_repeticao(lista_dados, numero_usuario)} vezes na lista de dados!")
 
 if __name__ == "__main__":
     main()
