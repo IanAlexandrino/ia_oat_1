92ade60855cd8525e42c47f8732a80f2086acd4b
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-23 10:11:39 -0300

    Criando dicionário com meses e as temperaturas em 0 e criando for para percorrer o dicionário e adicionar temperatura

--- problema_1/app.py
+++ problema_1/app.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+def main():
+    lista_meses_temperatura = [
+        {"Janeiro": 0, "Fevereiro": 0, "Março": 0, "Abril": 0, "Maio": 0, "Junho": 0, 
+         "Julho": 0, "Agosto": 0, "Setembro": 0, "Outubro": 0, "Novembro": 0, "Dezembro": 0}
+    ]
+
+    print("Digite a temperatura de ")
+
+    for meses_temperatura in lista_meses_temperatura:
+        for mes, temperatura in meses_temperatura.items():
+            meses_temperatura[mes] = input(mes + ": ")
+
+    print(lista_meses_temperatura)
+
+if __name__ == "__main__":
+    main()
