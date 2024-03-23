bf95111f04aa0cf2f76fb9c559e9763677142486
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-23 13:04:46 -0300

    Adicionando função para inserir dados na lista_dados

--- problema_2/app.py
+++ problema_2/app.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import random
+
+def insere_dados(lista):
+    if not lista:
+        return 0
+    while len(lista) < 100:
+        lista.append(random.randint(1,100))
+
+def main():
+    lista_dados = []
+
+    insere_dados(lista_dados)
+
+    
+
+if __name__ == "__main__":
+    main()
