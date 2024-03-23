ff9a69435ddd8aaa0f2446473f5c0ec8fd726e2d
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-23 13:21:00 -0300

    Criando 2 versão para a resolução do problema_2

---
+++ problema_2/app_v2.py
@@ -0,0 +1,19 @@
+import random
+
+def insere_dados(lista):
+    while len(lista) < 100:
+        lista.append(random.randint(1, 50))
+
+def main():
+    lista_dados = []
+
+    insere_dados(lista_dados)
+
+    print(lista_dados)
+
+    numero_usuario = int(input("Insira um número até 50 para procurar na lista de dados: "))
+
+    print(f"O número {numero_usuario} aparece {conta_repeticao(lista_dados, numero_usuario)} vezes na lista de dados!")
+
+if __name__ == "__main__":
+    main()
