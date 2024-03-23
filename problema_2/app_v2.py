c2b9401e11ae0191d76183e033d9f470d2b811ac
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-23 13:32:27 -0300

    Adicionando método conta_repeticões e finalizando main da app_v2.py

--- problema_2/app_v2.py
+++ problema_2/app_v2.py
@@ -4,16 +4,28 @@ def insere_dados(lista):
     while len(lista) < 100:
         lista.append(random.randint(1, 50))
 
+def conta_repeticoes(lista):
+    contagem = {}
+
+    for valor in lista:
+        if valor in contagem:
+            contagem[valor] += 1
+        else:
+            contagem[valor] = 1
+
+    return contagem
+
 def main():
     lista_dados = []
 
     insere_dados(lista_dados)
 
-    print(lista_dados)
+    contagem = conta_repeticoes(lista_dados)
 
-    numero_usuario = int(input("Insira um número até 50 para procurar na lista de dados: "))
+    for valor, frequencia in contagem.items():
+        print(f"O valor {valor} aparece {frequencia} vezes")
 
-    print(f"O número {numero_usuario} aparece {conta_repeticao(lista_dados, numero_usuario)} vezes na lista de dados!")
+    print(lista_dados)    
 
 if __name__ == "__main__":
     main()
